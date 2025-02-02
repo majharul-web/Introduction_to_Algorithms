ff8ff88bd4634a8594065e12db5092cf6009a5f1
majharul <majharuljony163@gmail.com>
2025-02-02 16:44:08 +0600

    learn:Adjacency List Implementation

--- graph-representation/adjacency_list_directed.cpp
+++ graph-representation/adjacency_list_directed.cpp
@@ -11,8 +11,8 @@ int main()
     {
         int a, b;
         cin >> a >> b;
-        adj_list[b].push_back(a);
-        }
+        adj_list[a].push_back(b);
+    }
 
     for (int i = 0; i < n; i++)
     {
