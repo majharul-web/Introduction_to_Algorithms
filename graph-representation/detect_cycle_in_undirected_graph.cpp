0522c6dc4c4cb74db402811a9bdce372aedd016d
majharul <majharuljony163@gmail.com>
2025-02-10 09:30:56 +0600

    Detect cycle in undirected graph

---
+++ graph-representation/detect_cycle_in_undirected_graph.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj_list[105];
+bool vis[105];
+int parent[105];
+
+bool cycle;
+
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    vis[src] = true;
+
+    while (!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+
+        for (int child : adj_list[par])
+        {
+            if (!vis[child] && parent[par] != child)
+            {
+                cycle = true;
+            }
+            if (!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+                parent[child] = par;
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    int n, e;
+    cin >> n >> e;
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
+    memset(vis, false, sizeof(vis));
+    memset(parent, -1, sizeof(parent));
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+        {
+            bfs(i);
+        }
+    }
+
+    if (cycle)
+    {
+        cout << "Cycle detected!";
+    }
+    else
+    {
+        cout << "No cycle";
+    }
+
+    return 0;
+}
+
+/*
+
+input:
+6 5
+0 1
+1 2
+2 3
+3 0
+4 5
+
+output: Cycle detected!
+
+*/
