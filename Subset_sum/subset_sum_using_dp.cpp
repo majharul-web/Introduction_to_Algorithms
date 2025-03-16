7e792548ae159ccda01f24bebe35b03fdd5ce17d
majharul <majharuljony163@gmail.com>
2025-03-16 12:32:52 +0600

    subset_sum_using_dp

--- Subset_sum/subset_sum_using_dp.cpp
+++ Subset_sum/subset_sum_using_dp.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 
 int val[1005];
+int dp[1005][1005];
 
-bool subset_sum(int i, int sum) // Complexity:O (2^N)
+bool subset_sum(int i, int sum) // Complexity:O (N * sum)
 {
 
     if (i < 0)
@@ -18,15 +19,22 @@ bool subset_sum(int i, int sum) // Complexity:O (2^N)
         }
     }
 
+    if (dp[i][sum] != -1)
+    {
+        return dp[i][sum];
+    }
+
     if (val[i] <= sum)
     {
         bool op1 = subset_sum(i - 1, sum - val[i]);
         bool op2 = subset_sum(i - 1, sum);
-        return op1 || op2;
+        dp[i][sum] = op1 || op2;
+        return dp[i][sum];
     }
     else
     {
-        return subset_sum(i - 1, sum);
+        dp[i][sum] = subset_sum(i - 1, sum);
+        return dp[i][sum];
     }
 }
 
@@ -41,6 +49,15 @@ int main()
 
     int sum;
     cin >> sum;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= sum; j++)
+        {
+            dp[i][j] = -1;
+        }
+    }
+
     if (subset_sum(n - 1, sum))
     {
         cout << "Yes";
@@ -52,3 +69,19 @@ int main()
 
     return 0;
 }
+
+/*
+input:
+4
+1 2 4 6
+15
+
+Output: No
+
+input:
+4
+1 2 4 6
+5
+
+Output: Yes
+*/
