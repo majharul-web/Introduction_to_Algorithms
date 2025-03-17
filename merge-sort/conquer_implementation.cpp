953bfb2e703ea6cb095949c14d58c697b9422f4f
majharul <majharuljony163@gmail.com>
2025-03-17 12:29:48 +0600

    Conquer Implementation

---
+++ merge-sort/conquer_implementation.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int m;
+    cin >> m;
+    int b[m];
+    for (int i = 0; i < m; i++)
+    {
+        cin >> b[i];
+    }
+
+    int c[n + m];
+    int i = 0, j = 0, curr = 0;
+
+    while (i < n && j < m)
+    {
+        if (a[i] < b[j])
+        {
+            c[curr++] = a[i++];
+        }
+        else
+        {
+            c[curr++] = b[j++];
+        }
+    }
+
+    while (i < n)
+    {
+        c[curr++] = a[i++];
+    }
+    while (j < m)
+    {
+        c[curr++] = b[j++];
+    }
+
+    for (int i = 0; i < n + m; i++)
+    {
+        cout << c[i] << " ";
+    }
+
+    return 0;
+}
+
+/*
+
+input
+3
+2 9 13
+4
+3 5 7 8
+
+output:
+2 3 5 7 8 9 13
+*/
